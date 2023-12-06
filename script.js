843a51ba17f3708afebfb67c429157347fb2299e
justin-ace <78005628+justin-ace@users.noreply.github.com>
2023-12-06 19:23:25 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Aaaaa. I love you too beb";
+    gif.src = "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
+    yesBtn.classList.add("d-none");
+    noBtn.classList.add("d-none");
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth = noBtnRect.width;
+    const maxY = window.innerHeight = noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX + 50);
+    const randomY = Math.floor(Math.random() * maxY + 50);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style,top = randomY + "px";
+});
