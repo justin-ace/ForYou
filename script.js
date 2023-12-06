e4fdb699695f62a95c41282bef5e5e27dfe2b48d
justin-ace <78005628+justin-ace@users.noreply.github.com>
2023-12-06 19:29:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ noBtn.addEventListener("mouseover", () => {
     const maxX = window.innerWidth = noBtnRect.width;
     const maxY = window.innerHeight = noBtnRect.height;
 
-    const randomX = Math.floor(Math.random() * maxX + 50);
-    const randomY = Math.floor(Math.random() * maxY + 50);
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
 
     noBtn.style.left = randomX + "px";
     noBtn.style,top = randomY + "px";
